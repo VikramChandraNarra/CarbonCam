31d3596af7b43e2825fd6f75ac1523a68050c8ee
Vikram Narra <vchandra.narra@gmail.com>
2022-10-15 16:01:07 -0400

    Second Commit

--- app/src/main/java/com/example/carboncam/MainActivity.java
+++ app/src/main/java/com/example/carboncam/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.content.Intent;
@@ -15,18 +18,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<ScannedItem> objects = new ArrayList<ScannedItem>();
 
-    ImageView imageview;
+//    ImageView imageview;
     Button btopen;
+    Button feedbtn;
+    Bitmap bm;
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycler_view);
 
-        imageview = findViewById(R.id.image_view);
+//        imageview = findViewById(R.id.image_view);
         btopen = findViewById(R.id.bt_open);
+        feedbtn = findViewById(R.id.profile);
+
 
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this,
@@ -40,9 +53,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 100);
+                startActivityForResult(intent, 0);
             }
         });
+        feedbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Feed.class);
+                startActivity(intent);
+            }
+        });
+
+        setUserInfo();
+        setAdapter();
+    }
+
+    private void setAdapter(){
+        MyAdapter adapter = new MyAdapter(objects);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setUserInfo(){
+        objects.add(new ScannedItem("eee", 4, bm));
+        objects.add(new ScannedItem("eee", 4, bm));
+        objects.add(new ScannedItem("eee", 4, bm));
+
     }
 
 
@@ -50,9 +88,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == 100) {
-            Bitmap captureImage = (Bitmap)data.getExtras().get("data");
-            imageview.setImageBitmap(captureImage);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (data != null) {
+                Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+                // Here is where you inputting the values of the ML algorihtm, from extrapolatign the data
+                ScannedItem o = new ScannedItem("hello", 4, captureImage);
+                objects.add(o);
+                System.out.println("haris");
+                setAdapter();
+            }
+
+
+
+//            imageview.setImageBitmap(captureImage);
         }
     }
 }
